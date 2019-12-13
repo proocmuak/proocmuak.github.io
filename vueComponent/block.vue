32deb567de59972503656e4a48d54ea8ddd94db5
proocmuak <proocmual@protonmail.com>
2019-12-13 19:50:44 +0700

    commit 1

---
+++ vueComponent/block.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="block">
+    <rightMenu></rightMenu>
+  </div>
+</template>
+
+<script>
+import rightMenu from "./rightMenu";
+export default {
+  name: "huynya",
+  components: { rightMenu }
+};
+</script>
+
+<style>
+.block {
+  font-family: "Playfair Display", serif;
+  background-image: url(../images/fon.jpg);
+  width: 100vw;
+  height: 100vh;
+  background-size: 100%;
+  background-repeat: no-repeat;
+  display: flex;
+  align-items: center;
+}
+.welcomeText {
+  width: 100px;
+  height: 200px;
+  color: azure;
+}
+</style>
---
+++ vueComponent/rightMenu.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="rightMenu">
+    <div class="labele"></div>
+    <div class="aboutOpenning">
+      <span class="think">Мы открываемся</span>
+      <br />
+      <span class="thick">21 никогда</span>
+    </div>
+    <div class="buttonLearnMore">Узнать больше</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "rightMenu"
+};
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.rightMenu {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  justify-content: space-evenly;
+  width: 95vw;
+  height: 50vh;
+}
+.labele {
+  background-image: url("../icon.png");
+  width: 100px;
+  height: 100px;
+  background-size: 100%;
+  background-repeat: no-repeat;
+}
+.think {
+  font-size: 50px;
+  font-weight: 300;
+  color: #fff;
+}
+.thick {
+  font-size: 50px;
+  font-weight: 900;
+  color: #fff;
+}
+.buttonLearnMore {
+  background-color: rgba(11, 95, 15, 0.74);
+  color: wheat;
+  width: 390px;
+  height: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 45px;
+  font-weight: 600;
+  border-radius: 10%;
+  position: relative;
+  cursor: pointer;
+}
+.buttonLearnMore:hover {
+  transition: 0.5s;
+  color: white;
+  width: 23vw;
+  height: 110px;
+  font-size: 50px;
+  font-weight: 800;
+}
+</style>
